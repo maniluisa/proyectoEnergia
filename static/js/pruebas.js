2b6435c84693ac5c1bb1820b962aba303a1d5ab2
maniluisa <saminsuas@gmail.com>
2024-12-05 21:04:23 -0500

    51224

---
+++ static/js/pruebas.js
@@ -0,0 +1,23 @@
+// alert('hola mundo')
+// var edad
+// let nombre = prompt('Ingrese tu nombre')
+// console.log(nombre)
+
+// let numero = parseInt(prompt('ingresa tu numero 1'));
+// let numero2 = parseInt(prompt('ingresa tu numero 2'));
+
+// alert(numero + numero2);
+
+// if (nombre =='german'){
+//     alert('Puedes ingresar')
+// }else{
+//     alert('No puedes ingresar')
+// }
+// para acceder al documento html hay clases, value hace referencia a la etiquetas, si no lo pone abarca todo query selector en vez de id sino con #
+
+let n1 =document.getElementById('num1').value
+let n2 = document.getElementById('num2').value
+
+document.getElementById('enviar').addEventListener('')
+
+console.log(n1)
